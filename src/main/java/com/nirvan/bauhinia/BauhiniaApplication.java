15a30960e5ba01c0dfa4101e02213dd00d01c2f9
Shalin Kulawardane <nirvan.work@gmail.com>
2023-06-03 00:48:35 +0530

    Initial commit

---
+++ src/main/java/com/nirvan/bauhinia/BauhiniaApplication.java
@@ -0,0 +1,13 @@
+package com.nirvan.bauhinia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BauhiniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BauhiniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirvan/bauhinia/BauhiniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirvan.bauhinia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BauhiniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
