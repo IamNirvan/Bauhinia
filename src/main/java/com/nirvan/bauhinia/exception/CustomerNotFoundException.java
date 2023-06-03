c155998e66af13d666bd16e03262700285c87cfc
Shalin Kulawardane <nirvan.work@gmail.com>
2023-06-04 04:13:33 +0530

    Added CustomerNotFoundExcepetion and InvalidParameterException

---
+++ src/main/java/com/nirvan/bauhinia/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.nirvan.bauhinia.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/nirvan/bauhinia/exception/InvalidParameterException.java
@@ -0,0 +1,6 @@
+package com.nirvan.bauhinia.exception;
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
