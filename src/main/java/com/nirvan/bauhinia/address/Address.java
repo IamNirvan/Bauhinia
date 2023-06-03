11ef4f8904aa16ec993e41746e95a69d03157109
Shalin Kulawardane <nirvan.work@gmail.com>
2023-06-04 04:36:15 +0530

    Added address service, repository and entity

--- src/main/java/com/nirvan/bauhinia/address/Address.java
+++ src/main/java/com/nirvan/bauhinia/address/Address.java
@@ -1,2 +1,41 @@
-package com.nirvan.bauhinia.address;public class Address {
+package com.nirvan.bauhinia.address;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "Address")
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "street_name", nullable = false)
+    private String streetName;
+
+    @Column(name = "nuilding_number", nullable = false)
+    private String buildingNumber;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    public Address(String province, String city, String streetName, String buildingNumber, String postalCode) {
+        this.province = province;
+        this.city = city;
+        this.streetName = streetName;
+        this.buildingNumber = buildingNumber;
+        this.postalCode = postalCode;
+    }
 }
--- src/main/java/com/nirvan/bauhinia/address/AddressRepository.java
+++ src/main/java/com/nirvan/bauhinia/address/AddressRepository.java
@@ -1,2 +1,6 @@
-package com.nirvan.bauhinia.address;public interface AddressRepository {
+package com.nirvan.bauhinia.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
