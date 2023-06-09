4a7e2b6f6ebbf2638cb006415c5b0933784e1811
Shalin Kulawardane <nirvan.work@gmail.com>
2023-06-09 14:11:00 +0530

    Modified custom exception handler and payload to deal with MethodArgumentNotValidException

--- src/main/java/com/nirvan/bauhinia/exception/ApiExceptionHandler.java
+++ src/main/java/com/nirvan/bauhinia/exception/ApiExceptionHandler.java
@@ -2,11 +2,14 @@ package com.nirvan.bauhinia.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 //
 // This annotation tells Spring that this class
@@ -24,7 +27,7 @@ public class ApiExceptionHandler {
      * This exception handler only handles exceptions that are
      * of type ApiRequestException (Custom exception).
      * */
-    public ResponseEntity<Object> handleException(ApiRequestException exception) {
+    public ResponseEntity<ExceptionPayload> handleCustomException(ApiRequestException exception) {
         //
         // Create the payload
         //
@@ -36,6 +39,22 @@ public class ApiExceptionHandler {
         //
         // Return the response entity with the payload and status code
         //
+        return ResponseEntity.badRequest().body(payload);
+    }
+
+    public ResponseEntity<ExceptionPayload> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        //
+        // Get the fields and their corresponding error messages and add them into the hash map
+        //
+        Map<String, String> errorMessageMapping = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(
+                (error) -> errorMessageMapping.put(error.getField(), error.getDefaultMessage())
+        );
+        ExceptionPayload payload = ExceptionPayload.builder()
+                .errorMessageMapping(errorMessageMapping)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .zonedDateTime(ZonedDateTime.now(ZoneId.of("Z")))
+                .build();
         return ResponseEntity.badRequest().body(payload);
     }
 }
